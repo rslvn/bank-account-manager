ae86f34933e7cb52d7d290cfc39bfbdd6b1695cb
rslvn <avan.resul@gmail.com>
2020-10-28 01:27:50 +0100

    circleci testcontainers

--- src/test/java/com/rabobank/bankaccountmanager/service/BankAccountServiceTest.java
+++ src/test/java/com/rabobank/bankaccountmanager/service/BankAccountServiceTest.java
@@ -13,13 +13,12 @@ import org.mockito.MockitoAnnotations;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.mockito.Mockito.verify;
+
 public class BankAccountServiceTest {
 
     @Mock
     private BankAccountRepository bankAccountRepository;
-
-    @Mock
-    private CustomerService customerService;
 
     @InjectMocks
     private BankAccountService bankAccountService;
@@ -32,7 +31,7 @@ public class BankAccountServiceTest {
 
     @Test
     public void testIncreaseCurrentBalance() {
-        BankAccount bankAccount = BankAccount.builder().id(1L).currentBalance(BigDecimal.TEN).build();
+        BankAccount bankAccount = BankAccount.builder().id(1L).currentBalance(BigDecimal.ONE).build();
 
         Mockito.when(bankAccountRepository.increaseCurrentBalance(bankAccount.getId(), BigDecimal.ONE))
                 .thenReturn(1);
@@ -40,6 +39,8 @@ public class BankAccountServiceTest {
                 .thenReturn(Optional.of(bankAccount));
 
         bankAccountService.increaseCurrentBalance(bankAccount, BigDecimal.ONE);
+
+        verify(bankAccountRepository).increaseCurrentBalance(bankAccount.getId(), BigDecimal.ONE);
     }
 
     @Test(expected = BankAccountManagerException.class)
@@ -50,6 +51,8 @@ public class BankAccountServiceTest {
                 .thenReturn(1);
 
         bankAccountService.increaseCurrentBalance(bankAccount, BigDecimal.ONE);
+
+        verify(bankAccountRepository).increaseCurrentBalance(bankAccount.getId(), BigDecimal.ONE);
     }
 
     @Test(expected = BankAccountManagerException.class)
@@ -68,6 +71,8 @@ public class BankAccountServiceTest {
                 .thenReturn(Optional.of(bankAccount));
 
         bankAccountService.decreaseCurrentBalance(bankAccount, BigDecimal.ONE);
+
+        verify(bankAccountRepository).decreaseCurrentBalance(bankAccount.getId(), BigDecimal.ONE);
     }
 
     @Test(expected = BankAccountManagerException.class)
